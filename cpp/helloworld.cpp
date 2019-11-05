7bc683c1cf6a9d10e37bdbb1dc658a3ae2592fde
wangding <276284834@qq.com>
2019-11-05 22:00:32 +0800

    helloworld

--- cpp/helloworld.cpp
+++ cpp/helloworld.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+template<typename T>
 
-int main(){
-    cout << "helloworld" <<endl;
+T min(T a,T b,T c)
+{
+    if(a > b)
+    a = b;
+    if(a > c)
+    a = c;
+    return a;
+}
+
+int main()
+{
+    int a = 4,b = 2,c = 3;
+    cout << min(a, b, c) <<endl;
+    return 0;
 }
