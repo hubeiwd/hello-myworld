554134d8dfc073a6efe811beaa423b3b4fe01780
wangding <276284834@qq.com>
2019-11-06 22:07:18 +0800

    string

--- c/helloworld.c
+++ c/helloworld.c
@@ -1,6 +1,20 @@
-#include "stdio"
+#include "stdio.h"
 
 int main()
 {
-    printf("helloworld");
+    int *p1 ,(*p2)[10];
+    int aa[10] = {0};
+    int *bb[10] = {0};
+    char *str = "abc";
+    str[0] = 'd';
+    p1 = aa;
+    p2 = &aa;
+    int a = 4,b = 2,c = 3;
+    //cout << min(a, b, c) << endl;
+    //cout << *p1 << endl;
+    //cout << p2[0] << endl;
+    ////cout << bb + 1 << endl;
+    printf("the letter: %s",str);
+    //cout << str[0] << endl;
+    return 0;
 }
--- cpp/helloworld.cpp
+++ cpp/helloworld.cpp
@@ -13,7 +13,18 @@ T min(T a,T b,T c)
 
 int main()
 {
+    int *p1 ,(*p2)[10];
+    int aa[10] = {0};
+    int *bb[10] = {0};
+    char *str = "abc";
+    //*str = 'd';
+    p1 = aa;
+    p2 = &aa;
     int a = 4,b = 2,c = 3;
-    cout << min(a, b, c) <<endl;
+    cout << min(a, b, c) << endl;
+    cout << *p1 << endl;
+    cout << p2[0] << endl;
+    cout << bb + 1 << endl;
+    cout << str[1] << endl;
     return 0;
 }
