c92c831ebf667488d61619b4ca32b522eb9853a0
wangding <276284834@qq.com>
2019-11-13 22:28:43 +0800

    duiqi

--- cpp/enum.cpp
+++ cpp/enum.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 using namespace std;
-template<typename T>
 
-T min(T a,T b,T c)
-{
-    if(a > b)
-    a = b;
-    if(a > c)
-    a = c;
-    return a;
-}
+#define TEST(x) {x++;x++;}
 
-struct student{
-    int num;
-    char name[20];
+#pragma pack(4)
+union A{
+    int a[5];
+    char b;
+    double c;
 };
+#pragma pack(8)
+struct B{
+    char a;
+    double b;
+    int c;
+}test_struct_b;
+
 
 int main()
 {
-    int a = 4,b = 2,c = 3;
-    int (*ptr)(int x, int y, int z);
-    ptr = &min;
-    cout << (*ptr)(a, b, c) << endl;
+    int x = 0, i = 1;
+    if(i < 0)
+        TEST(x)
+    else
+        i = 2;
+    cout << x << endl;
+    cout << sizeof(A) << endl;
+    cout << sizeof(test_struct_b) << endl;
     return 0;
 }
