6b3aa760e5d57982c1965a2668799c0cf96156b4
wangding <276284834@qq.com>
2019-11-13 22:28:43 +0800

    duiqi

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 print("helloworld")
+
+x = {'username': 'admin', 'machines': ['foo', 'bar', 'baz']}
+y = x.copy()
+y['username'] = 'mlh'
+y['machines'].remove('bar')
+print(y)
+print(x)
