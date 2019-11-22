ca9a5f7f45e1711e718e41fcaa0ea5431b716e24
wangding <276284834@qq.com>
2019-11-22 22:46:16 +0800

    net

--- python/net.py
+++ python/net.py
@@ -7,7 +7,7 @@ s.bind((host, port))
 s.listen(10)
 
 while True:
-    c,addr = s.accept
+    c,addr = s.accept()
     print('get connection from', addr)
     c.send('thank you for connecting')
     c.close()
