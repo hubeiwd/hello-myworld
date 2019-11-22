06e9feae483ff77fd8de15ff18d0e3bd9e061ce6
wangding <276284834@qq.com>
2019-11-22 22:23:32 +0800

    python_server

---
+++ python/net.py
@@ -0,0 +1,13 @@
+import socket
+s = socket.socket()
+
+host  = socket.gethostname()
+port = 2356
+s.bind((host, port))
+s.listen(10)
+
+while True:
+    c,addr = s.accept
+    print('get connection from', addr)
+    c.send('thank you for connecting')
+    c.close()
