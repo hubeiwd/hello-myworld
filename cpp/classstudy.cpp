dda54cfeb529d234b1abde8ff9b31eb8feaf20a6
wangding <276284834@qq.com>
2019-11-28 21:40:36 +0800

    cpp

--- cpp/classstudy.cpp
+++ cpp/classstudy.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Time{
     public:
         Time();
+        Time(int hour, int minutes, int second);
         // set_time();
         // get_time();
     private:
@@ -16,8 +17,11 @@ Time::Time(){
     second=0;
 }
 
+Time::Time(int hour, int minutes, int second):hour(hour),minute(minute),second(second) {}
+
 int main()
 {
     Time test;
+    Time test2(5, 5, 5);
     return 0;
 }
